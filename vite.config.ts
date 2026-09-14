74f96ab0ac6d504db9ea22410d5d5b67ea321870
kitsun3kage <kitsun3.kage@gmail.com>
2026-09-14 17:03:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/Spotify-Downloader/'
-    : '/',
+
+  base: '/Spotify-Downloader/',
+
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
-});
+    sourcemap: false,
+  },
+})
