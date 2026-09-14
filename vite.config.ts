814e51edf011783817334d19f4c16fa2bb0925a8
kitsun3kage <kitsun3.kage@gmail.com>
2026-09-14 16:57:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-{
-  "name": "spotify-downloader",
-  "private": true,
-  "version": "1.0.0",
-  "type": "module",
-  "scripts": {
-    "dev": "vite",
-    "build": "tsc -b && vite build",
-    "preview": "vite preview"
-  },
-  "dependencies": {
-    "lucide-react": "^0.468.0",
-    "react": "^18.3.1",
-    "react-dom": "^18.3.1"
-  },
-  "devDependencies": {
-    "@types/react": "^18.3.12",
-    "@types/react-dom": "^18.3.1",
-    "@vitejs/plugin-react": "^4.3.4",
-    "typescript": "^5.7.2",
-    "vite": "^6.0.5"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/Spotify-Downloader/'
+    : '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
-}
+});
